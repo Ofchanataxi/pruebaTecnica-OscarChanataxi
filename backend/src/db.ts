e8715290528c670b1fe95cb819f6878fe7476cb9
ofchanataxi <ofchanataxi@espe.edu.ec>
2025-09-04 19:27:50 -0500

    Fix: Credenciales de la base datos

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -5,8 +5,8 @@ dotenv.config();
 
 export const pool = new Pool({
   host: process.env.DB_HOST || "localhost",
-  user: process.env.DB_USER || "postgres",
-  password: process.env.DB_PASS || "1234",
-  database: process.env.DB_NAME || "pozosdb",
+  user: process.env.DB_USER || "pozos_user",
+  password: process.env.DB_PASS || "pozos_password",
+  database: process.env.DB_NAME || "pozos_db",
   port: Number(process.env.DB_PORT) || 5432,
 });
